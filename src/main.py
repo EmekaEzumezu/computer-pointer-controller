17269e15f7664ddbfe316fa1411fd8fdd82b1fb5
Chukwuemeka Ezumezu <emyfrancis2012@gmail.com>
2020-07-05 04:58:38 +0000

    Edit main.py to save out image

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ import os
 import time
 import cv2
 import numpy as np
+import logging
+
+import math
 import logging as log
 
 from face_detection import FaceDetectionModel
@@ -52,149 +55,173 @@ def build_argparser():
 
 def infer_on_stream(args):
     
-    input_file_path = args.input # Path to image or video file
-    logger = log.getLogger()
-    previewFlags = args.previewFlags
+    try:
     
-    input_feeder = input_feeder_func(input_file_path)
-    
-    face_detection_instant, head_pose_estimation_instant, facial_landmarks_instant, gaze_estimation_instant = \
-    model_instants(args)
+        # Logger
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.FileHandler("time-stats.log"),
+                logging.StreamHandler()
+            ])
 
-#     # Checks for live feed
-#     if input_file_path == 'CAM':
-#         input_feeder = InputFeeder("cam")
+        input_file_path = args.input # Path to image or video file
+        logger = log.getLogger()
+        previewFlags = args.previewFlags
 
-#     # Checks for video file
-#     else:
-#         input_feeder = InputFeeder("video", input_file_path)
-#         assert os.path.isfile(input_file_path), "Specified input file doesn't exist"
+        input_feeder = input_feeder_func(input_file_path)
+
+        face_detection_instant, head_pose_estimation_instant, facial_landmarks_instant, gaze_estimation_instant = \
+        model_instants(args)
+
+        ### Loading and logging the model load time ###
+        logging.info("========== Models Load time ==========") 
+        start_time = time.time()
+        face_detection_instant.load_model()
+        logging.info("Face Detection Model: {:.1f}ms".format(1000 * (time.time() - start_time)) )
+
+        start_time = time.time()
+        head_pose_estimation_instant.load_model()
+        logging.info("Headpose Estimation Model: {:.1f}ms".format(1000 * (time.time() - start_time)) )
+
+        start_time = time.time()
+        facial_landmarks_instant.load_model()
+        logging.info("Facial Landmarks Detection Model: {:.1f}ms".format(1000 * (time.time() - start_time)) )
+
+        start_time = time.time()
+        gaze_estimation_instant.load_model()
+        logging.info("Gaze Estimation Model: {:.1f}ms".format(1000 * (time.time() - start_time)) )
+        logging.info("========== End ==========") 
+
+        # Open video capture
+        input_feeder.load_data()
+
+
+        # init scene variables
+        frame_count = 0
+
+        # Initialize inference time
+        face_detection_infer_time = 0
+        facial_landmarks_infer_time = 0
+        head_pose_estimation_infer_time = 0
+        gaze_estimation_infer_time = 0
+
+    #    counter = 0
+    #    start_inf_time = time.time()
+        #logger.error("Start inferencing on input video.. ")
         
-        
-#     model_paths_dict = {'face_detection':args.face_detection,
-#                         'head_pose_estimation':args.head_pose_estimation,
-#                         'facial_landmarks_detection':args.facial_landmarks_detection,
-#                         'gaze_estimation':args.gaze_estimation}
-
-#     face_detection_instant = FaceDetectionModel(model_name=args.face_detection,
-#                                                 device=args.device, threshold=args.prob_threshold,
-#                                                 extensions=args.cpu_extension)
-    
-#     head_pose_estimation_instant = HeadPoseEstimationModel(model_name=args.head_pose_estimation,
-#                                                            device=args.device, 
-#                                                            extensions=args.cpu_extension)
-
-#     facial_landmarks_instant = FacialLandmarksDetectionModel(model_name=args.facial_landmarks_detection,
-#                                                              device=args.device,
-#                                                              extensions=args.cpu_extension)
-
-#     gaze_estimation_instant = GazeEstimationModel(model_name=args.gaze_estimation, 
-#                                                   device=args.device, 
-#                                                   extensions=args.cpu_extension)
-    
-#     mouse_controller_instant = MouseController('medium', 'fast')
-
-
-    face_detection_instant.load_model()
-    
-    head_pose_estimation_instant.load_model()
-    
-    facial_landmarks_instant.load_model()
-    
-    gaze_estimation_instant.load_model()
-    
-    input_feeder.load_data()
-    
-    
-    counter = 0
-    start_inf_time = time.time()
-    logger.error("Start inferencing on input video.. ")
-    for flag, frame in input_feeder.next_batch():
-        if not flag:
-            break
-        pressed_key = cv2.waitKey(60)
-        counter = counter + 1
-        face_coordinates, face_image = face_detection_instant.predict(frame.copy())
-
-#         if face_coordinates == 0:
-#             continue
-            
-        if type(face_image)==int:
-            logger.error("Unable to detect the face.")
-            if key==27:
+        ### Loop until stream is over ###
+        for flag, frame in input_feeder.next_batch():
+            if not flag:
                 break
-            continue
+            pressed_key = cv2.waitKey(60)
 
-        head_pose_estimation_model_output = head_pose_estimation_instant.predict(face_image)
+            frame_count += 1
+    #        counter = counter + 1
 
-        left_eye_image, right_eye_image, eye_coord = facial_landmarks_instant.predict(face_image)
+            start_time = time.time()
+            face_coordinates, face_image = face_detection_instant.predict(frame.copy())
+            face_detection_infer_time += time.time() - start_time
 
-        mouse_coordinate, gaze_vector = gaze_estimation_instant.predict(left_eye_image, right_eye_image,
-                                                                             head_pose_estimation_model_output)
+    #         if face_coordinates == 0:
+    #             continue
 
-        if len(previewFlags) != 0:
-            preview_window = frame.copy()
-            if 'fd' in previewFlags:
-                if len(previewFlags) != 1:
-                    preview_window = face_image
-                else:
-                    cv2.rectangle(preview_window, (face_coordinates[0], face_coordinates[1]),
-                                  (face_coordinates[2], face_coordinates[3]), (0, 150, 0), 3)
-            if 'fl' in previewFlags:
-                if not 'fd' in previewFlags:
-                    preview_window = face_image.copy()
-                cv2.rectangle(preview_window, (eye_coord[0][0], eye_coord[0][1]), (eye_coord[0][2], eye_coord[0][3]),
-                              (150, 0, 150))
-                cv2.rectangle(preview_window, (eye_coord[1][0], eye_coord[1][1]), (eye_coord[1][2], eye_coord[1][3]),
-                              (150, 0, 150))
-            if 'hp' in previewFlags:
-                cv2.putText(preview_window,
-                            "yaw:{:.1f} | pitch:{:.1f} | roll:{:.1f}".format(head_pose_estimation_model_output[0],
-                                                                             head_pose_estimation_model_output[1],
-                                                                             head_pose_estimation_model_output[2]),
-                            (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.35, (0, 0, 0), 1)
-            if 'ge' in previewFlags:
+            if type(face_image)==int:
+                logger.error("Unable to detect the face.")
+                if key==27:
+                    break
+                continue
 
-                yaw = head_pose_estimation_model_output[0]
-                pitch = head_pose_estimation_model_output[1]
-                roll = head_pose_estimation_model_output[2]
-                focal_length = 950.0
-                scale = 50
-                center_of_face = (face_image.shape[1] / 2, face_image.shape[0] / 2, 0)
-                if 'fd' in previewFlags or 'fl' in previewFlags:
-                    draw_axes(preview_window, center_of_face, yaw, pitch, roll, scale, focal_length)
-                else:
-                    draw_axes(frame, center_of_face, yaw, pitch, roll, scale, focal_length)
+            start_time = time.time()
+            head_pose_output = head_pose_estimation_instant.predict(face_image)
+            head_pose_estimation_infer_time += time.time() - start_time
 
-        if len(previewFlags) != 0:
-            img_hor = np.hstack((cv2.resize(frame, (500, 500)), cv2.resize(preview_window, (500, 500))))
-        else:
-            img_hor = cv2.resize(frame, (500, 500))
+            start_time = time.time()
+            left_eye, right_eye, eye_coords = facial_landmarks_instant.predict(face_image)
+            facial_landmarks_infer_time += time.time() - start_time
 
-        cv2.imshow('Visualization', img_hor)
-        mouse_controller_object.move(mouse_coordinate[0], mouse_coordinate[1])
+            start_time = time.time()
+            new_mouse_coords, gaze_vector = gaze_estimation_instant.predict(left_eye, right_eye,
+                                                                                 head_pose_output)
+            gaze_estimation_infer_time += time.time() - start_time
 
-        if pressed_key == 27:
-            logger.error("exit key pressed..")
-            break
-    inference_time = round(time.time() - start_inf_time, 1)
-    fps = int(counter) / inference_time
-    logger.error("counter {} seconds".format(counter))
-    logger.error("total inference time {} seconds".format(inference_time))
-    logger.error("fps {} frame/second".format(fps))
-    logger.error("Video has ended")
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'stats.txt'), 'w') as f:
-        f.write(str(inference_time) + '\n')
-        f.write(str(fps) + '\n')
-        f.write(str(load_total_time) + '\n')
+            if len(previewFlags) != 0:
+                preview_window = frame.copy()
+                if 'fd' in previewFlags:
+                    if len(previewFlags) != 1:
+                        preview_window = face_image
+                    else:
+                        cv2.rectangle(preview_window, (face_coordinates[0], face_coordinates[1]),
+                                      (face_coordinates[2], face_coordinates[3]), (0, 150, 0), 3)
+                if 'fl' in previewFlags:
+                    if not 'fd' in previewFlags:
+                        preview_window = face_image.copy()
+                    cv2.rectangle(preview_window, (eye_coords[0][0], eye_coords[0][1]), (eye_coords[0][2], eye_coords[0][3]),
+                                  (150, 0, 150))
+                    cv2.rectangle(preview_window, (eye_coords[1][0], eye_coords[1][1]), (eye_coords[1][2], eye_coords[1][3]),
+                                  (150, 0, 150))
+                if 'hp' in previewFlags:
+                    cv2.putText(preview_window,
+                                "yaw:{:.1f} | pitch:{:.1f} | roll:{:.1f}".format(head_pose_output[0],
+                                                                                 head_pose_output[1],
+                                                                                 head_pose_output[2]),
+                                (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.35, (0, 0, 0), 1)
+                if 'ge' in previewFlags:
 
-    #logger.error("VideoStream ended...")
-    input_feeder.close()
-    cv2.destroyAllWindows()
+                    yaw = head_pose_output[0]
+                    pitch = head_pose_output[1]
+                    roll = head_pose_output[2]
+                    focal_length = 950.0
+                    scale = 50
+                    center_of_face = (face_image.shape[1] / 2, face_image.shape[0] / 2, 0)
+                    if 'fd' in previewFlags or 'fl' in previewFlags:
+                        draw_axes(preview_window, center_of_face, yaw, pitch, roll, scale, focal_length)
+                    else:
+                        draw_axes(frame, center_of_face, yaw, pitch, roll, scale, focal_length)
+
+            if len(previewFlags) != 0:
+                img_hor = np.hstack((cv2.resize(frame, (500, 500)), cv2.resize(preview_window, (500, 500))))
+            else:
+                img_hor = cv2.resize(frame, (500, 500))
+                
+            
+            # Saving the image 
+            cv2.imwrite('output_images/demo.png', img_hor)
+            
+            
+            #cv2.imshow('Visualization', img_hor)
+            #mouse_controller_object.move(new_mouse_coords[0], new_mouse_coords[1])
+
+            if pressed_key == 27:
+                logger.error("exit key pressed..")
+                break
+#        inference_time = round(time.time() - start_inf_time, 1)
+    #    fps = int(counter) / inference_time
+    #     logger.error("counter {} seconds".format(counter))
+    #     logger.error("total inference time {} seconds".format(inference_time))
+    #     logger.error("fps {} frame/second".format(fps))
+    #     logger.error("Video has ended")
+#         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'stats.txt'), 'w') as f:
+#             f.write(str(inference_time) + '\n')
+#             f.write(str(fps) + '\n')
+#             f.write(str(load_total_time) + '\n')
+
+        #logging inference times
+        if(frame_count > 0):
+            logging.info("========== Models Inference time ==========") 
+            logging.info("Face Detection Model:{:.1f}ms".format(1000* face_detection_infer_time/frame_count))
+            logging.info("Facial Landmarks Detection Model:{:.1f}ms".format(1000* facial_landmarks_infer_time/frame_count))
+            logging.info("Headpose Estimation Model:{:.1f}ms".format(1000* head_pose_estimation_infer_time/frame_count))
+            logging.info("Gaze Estimation Model:{:.1f}ms".format(1000* gaze_estimation_infer_time/frame_count))
+            logging.info("========== End ==========") 
+
+        #logger.error("VideoStream ended...")
+        input_feeder.close()
+        cv2.destroyAllWindows()
     
-    #logger.error("VideoStream ended...")
-    #cv2.destroyAllWindows()
-    #inputFeeder.close()
+    except Exception as e:
+        logging.exception("Error running inference:" + str(e))
 
 # NOTE: draw_axes and build_camera_matrix code implementation gotten from 
 # https://knowledge.udacity.com/questions/171017    
@@ -270,10 +297,6 @@ def input_feeder_func(input_file_path):
     return input_feeder
 
 def model_instants(args):
-#     model_paths_dict = {'face_detection':args.face_detection,
-#                         'head_pose_estimation':args.head_pose_estimation,
-#                         'facial_landmarks_detection':args.facial_landmarks_detection,
-#                         'gaze_estimation':args.gaze_estimation}
 
     face_detection_instant = FaceDetectionModel(model_name=args.face_detection,
                                                 device=args.device, threshold=args.prob_threshold,
